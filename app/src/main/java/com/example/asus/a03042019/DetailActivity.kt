738848bffa7d601a6e7fa8f0dec4536e05897dac
movie <47974363+rabiakama@users.noreply.github.com>
2019-04-12 16:35:33 +0300

    düzenlendi

--- app/src/main/java/com/example/asus/a03042019/DetailActivity.kt
+++ app/src/main/java/com/example/asus/a03042019/DetailActivity.kt
@@ -8,14 +8,6 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.RequiresApi
-import android.support.design.widget.AppBarLayout
-import android.support.design.widget.CollapsingToolbarLayout
-import android.support.design.widget.FloatingActionButton
-import android.widget.TextView
-import android.widget.Toolbar
-import android.support.v7.widget.RecyclerView
-import android.widget.ImageView
-import com.bumptech.glide.Glide
 import android.support.v4.content.ContextCompat
 import android.view.View
 import com.example.asus.a03042019.model.Movies
--- app/src/main/java/com/example/asus/a03042019/utils/View.kt
+++ app/src/main/java/com/example/asus/a03042019/utils/View.kt
@@ -7,22 +7,22 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun clearLightStatusBar(view: View) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        var flags = view.systemUiVisibility
-        flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-        view.systemUiVisibility = flags
-    }
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+    var flags = view.systemUiVisibility
+    flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+    view.systemUiVisibility = flags
+  }
 }
 
 fun showKeyboard(view: View) {
-    val inputManager = view.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-    inputManager?.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+  val inputManager = view.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+  inputManager?.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
 }
 
 fun hideKeyboard(activity: Activity) {
-    val view = activity.findViewById<View>(android.R.id.content)
-    if (view != null) {
-        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+  val view = activity.findViewById<View>(android.R.id.content)
+  if (view != null) {
+    val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+  }
 }
